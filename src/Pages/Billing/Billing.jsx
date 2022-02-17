89a22c3e4844d4ecebbf0dd05bffd345e5322504
DToxVanilla <dtoxvanilla@gmail.com>
2022-02-17 23:13:13 +0000

    some extraoptimization

--- src/Pages/Billing/Billing.jsx
+++ src/Pages/Billing/Billing.jsx
@@ -1,47 +1,49 @@
+import { useEffect } from "react";
 import classes from "./Billing.module.css";
 
 const Billing = () => {
-  return (
-    <>
-      <h1>Copy Address Button</h1>
 
-      <div class="wrap">
-        <div class="shipping">
-          <h2>Shipping Address</h2>
-          <p class="checkBoxText">We will ship documents here</p>
-          <input placeholder="Name" class="nameShip" value="Will Robinson" />
+  return (
+
+    <>
+
+      <div className="wrap">
+        <div className="shipping">
+          <h2 className="billingH2">Shipping Address</h2>
+          <p className="checkBoxText">We will ship documents here</p>
+          <input placeholder="Name" className="nameShip" value="Will Robinson" />
           <input
             placeholder="Address 1"
-            class="address1Ship"
+            className="address1Ship"
             value="Tower Bridge Way"
           />
-          <input placeholder="Address 2" class="address2Ship" value="12" />
-          <input placeholder="City" class="cityShip" value="London" />
-          <select placeholder="County" class="stateShip">
+          <input placeholder="Address 2" className="address2Ship" value="12" />
+          <input placeholder="City" className="cityShip" value="London" />
+          <select placeholder="County" className="stateShip">
             <option value="Essex">Essex</option>
             <option value="Cambridge">Cambridge</option>
           </select>
-          <input placeholder="Zip" class="zipShip" value="E65RR" />
-          <input placeholder="Phone" class="phoneShip" value="(810) 758-9856" />
-          <input placeholder="Email" class="emailShip" value="lost@space.com" />
+          <input placeholder="Zip" className="zipShip" value="E65RR" />
+          <input placeholder="Phone" className="phoneShip" value="(810) 758-9856" />
+          <input placeholder="Email" className="emailShip" value="lost@space.com" />
         </div>
 
-        <div class="billing">
-          <h2>Billing Address</h2>
-          <input type="checkbox" class="checkBox" />
-          <p class="checkBoxText">Billing is the same as shipping</p>
-          <input placeholder="Name" class="nameBill" />
-          <input placeholder="Address 1" class="address1Bill" />
-          <input placeholder="Address 2" class="address2Bill" />
-          <input placeholder="City" class="cityBill" />
-          <select placeholder="State" class="stateBill">
+        <div className="billing">
+          <h2 className="billingH2">Billing Address</h2>
+          <input type="checkbox" className="checkBox" />
+          <p className="checkBoxText">Billing is the same as shipping</p>
+          <input placeholder="Name" className="nameBill" />
+          <input placeholder="Address 1" className="address1Bill" />
+          <input placeholder="Address 2" className="address2Bill" />
+          <input placeholder="City" className="cityBill" />
+          <select placeholder="State" className="stateBill">
             <option value="0">Select a county</option>
             <option value="Essex">Essex</option>
             <option value="Cambridge">Cambridge</option>
           </select>
-          <input placeholder="Zip" class="zipBill" />
-          <input placeholder="Phone" class="phoneBill" />
-          <input placeholder="Email" class="emailBill" />
+          <input placeholder="Zip" className="zipBill" />
+          <input placeholder="Phone" className="phoneBill" />
+          <input placeholder="Email" className="emailBill" />
         </div>
       </div>
     </>
